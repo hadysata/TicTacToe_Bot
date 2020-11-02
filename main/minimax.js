53b5ef70839f12d346b92ebc453048ebdabe7714
Hady <hadysata@gmail.com>
2020-11-02 06:23:10 +0300

    Implment the minimax

---
+++ main/minimax.js
@@ -0,0 +1,98 @@
+const huPlayer = "O";
+const aiPlayer = "X";
+
+function play(board = [0, 1, 2, 3, 4, 5, 6, 7, 8], humanMove) {
+    if (board.includes(humanMove)) {
+        board[minimax(board, aiPlayer).index] = aiPlayer;
+        return { 'board': board, 'whoseWon': whoseWon(board) };
+    } else {
+        return null;
+    }
+}
+
+function whoseWon(board) {
+    const isHumanWinning = winning(board, huPlayer);
+    const isAiWinning = winning(board, aiPlayer);
+    const isTie = emptyIndexes(board).length <= 0 && !isHumanWinning && !isAiWinning;
+    return isHumanWinning ? 'human' : isAiWinning ? 'ai' : isTie ? 'tie' : 'nobody';
+}
+
+function minimax(newBoard, player) {
+
+    var availSpots = emptyIndexes(newBoard);
+
+    if (winning(newBoard, huPlayer)) {
+        return { score: -10 };
+    }
+    else if (winning(newBoard, aiPlayer)) {
+        return { score: 10 };
+    }
+    else if (availSpots.length === 0) {
+        return { score: 0 };
+    }
+
+    var moves = [];
+
+    for (var i = 0; i < availSpots.length; i++) {
+        var move = {};
+        move.index = newBoard[availSpots[i]];
+
+        newBoard[availSpots[i]] = player;
+
+        if (player == aiPlayer) {
+            var result = minimax(newBoard, huPlayer);
+            move.score = result.score;
+        }
+        else {
+            var result = minimax(newBoard, aiPlayer);
+            move.score = result.score;
+        }
+
+        newBoard[availSpots[i]] = move.index;
+
+        moves.push(move);
+    }
+
+    var bestMove;
+    if (player === aiPlayer) {
+        var bestScore = -10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+
+        var bestScore = 10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+
+    return moves[bestMove];
+}
+
+function emptyIndexes(board) {
+    return board.filter(s => s != "O" && s != "X");
+}
+
+function winning(board, player) {
+    if (
+        (board[0] == player && board[1] == player && board[2] == player) ||
+        (board[3] == player && board[4] == player && board[5] == player) ||
+        (board[6] == player && board[7] == player && board[8] == player) ||
+        (board[0] == player && board[3] == player && board[6] == player) ||
+        (board[1] == player && board[4] == player && board[7] == player) ||
+        (board[2] == player && board[5] == player && board[8] == player) ||
+        (board[0] == player && board[4] == player && board[8] == player) ||
+        (board[2] == player && board[4] == player && board[6] == player)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
